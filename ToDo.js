0b979bf616ca1e5e65c9812983f388efa854595d
mingyumkim93 <mingyumkim93@gmail.com>
2019-12-17 13:35:15 +0200

    emojis for different more

--- ToDo.js
+++ ToDo.js
@@ -1,22 +1,48 @@
 import React, { Component } from "react";
 import { View, Text, TouchableOpacity, StyleSheet, Dimensions } from "react-native";
 
-const { width, height} = Dimensions.get("window");
+const { width, height } = Dimensions.get("window");
 
 export default class ToDo extends Component {
-    state= {
+    state = {
         isEditing: false,
         isCompleted: false
     };
 
     render() {
-        const {isCompleted} = this.state;
+        const { isEditing, isCompleted } = this.state;
         return (
             <View style={styles.container}>
+
+                <View style={styles.column}>
                 <TouchableOpacity onPress={this._toggleComplete}>
-                    <View style={[styles.circle, isCompleted? styles.completedCircle : styles.uncompletedCircle]}/>
+                    <View style={[styles.circle, isCompleted ? styles.completedCircle : styles.uncompletedCircle]} />
                 </TouchableOpacity>
-                <Text style={styles.text}>Hello I'm a To Do</Text>
+                <Text style={[styles.text, isCompleted ? styles.completedText : styles.uncompletedText]}>Hello I'm a To Do</Text>
+                </View>
+
+                {isEditing ? (
+                    <View style={styles.actions}>
+                        <TouchableOpacity onPressOut={this._finishEditing}>
+                            <View style={styles.actionContainer}>
+                                <Text style={styles.actionText}>✔</Text>
+                            </View>
+                        </TouchableOpacity>
+                    </View>
+                ) : (
+                        <View style={styles.actions}>
+                            <TouchableOpacity onPressOut={this._startEditing}>
+                                <View style={styles.actionContainer}>
+                                    <Text style={styles.actionText}>✎</Text>
+                                </View>
+                            </TouchableOpacity>
+                            <TouchableOpacity>
+                                <View style={styles.actionContainer}>
+                                    <Text style={styles.actionText}>❌</Text>
+                                </View>
+                            </TouchableOpacity>
+                        </View>
+                    )} 
             </View>
         )
     }
@@ -26,34 +52,63 @@ export default class ToDo extends Component {
                 isCompleted: !prevState.isCompleted
             }
         })
-    }
+    };
+
+    _startEditing = () => {
+        this.setState({isEditing:true})
+    };
+
+    _finishEditing = () => {
+        this.setState({isEditing:false})
+    };
 }
 
 const styles = StyleSheet.create({
-    container:{
-        width:width - 50,
+    container: {
+        width: width - 50,
         borderBottomColor: "#bbb",
         borderBottomWidth: StyleSheet.hairlineWidth,
         flexDirection: "row",
-        alignItems: "center"
+        alignItems: "center",
+        justifyContent:"space-between"
     },
-    circle:{
-        width:30,
-        height:30,
-        borderRadius:15,
+    circle: {
+        width: 30,
+        height: 30,
+        borderRadius: 15,
         borderWidth: 3,
         marginRight: 20
     },
-    completedCircle:{
-        borderColor:"red",
+    completedCircle: {
+        borderColor: "#bbb",
     },
-    uncompletedCircle:{
-        borderColor:"#bbb",
+    uncompletedCircle: {
+        borderColor: "red",
     },
-    text:{
-        fontWeight:"600",
-        fontSize:20,
+    text: {
+        fontWeight: "600",
+        fontSize: 20,
         marginVertical: 20
     },
-    
+    completedText: {
+        color: "#bbb",
+        textDecorationLine: "line-through"
+    },
+    uncompletedText: {
+        color: "#353839"
+    },
+    column: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+        width:width / 2,
+    },
+    actions:{
+        flexDirection:"row"
+    },
+    actionContainer:{
+        marginVertical:10,
+        marginHorizontal:10
+    }
+
 })
